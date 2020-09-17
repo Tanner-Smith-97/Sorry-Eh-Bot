23b779a4d85033fe213b71f83ac15fcf62afb845
Tanner S <smithjtanner@gmail.com>
2020-09-17 05:33:53 -0600

    is broken now
    Could not fix the mongo db problems

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const Discord = require('discord.js');
+const { mongo } = require('mongoose');
+
 
 const bot = new Discord.Client();
 
+//users
 const Landon = "140205912727093248";
 const Tanner = "379807275877138443";
 
+//roles
+const rolePoutine = "481987772043755530";
+
 const commandCooldown = new Set();
 const landonCooldown = new Set();
-//var d = new Date(); //current time
 
 let token = process.env.discordToken;
+
+// Retrieve
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.connectionString;
+const client = new MongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 
@@ -23,10 +34,9 @@ bot.on('ready', () => {
 bot.on('guildMemberAdd', member => {
     const channel = member.guild.channels.cache.find(ch => ch.name === 'spam');
 
-    if(!channel)
-    { 
-    console.log("Could not find Spam") 
-    return;
+    if (!channel) {
+        console.log("Could not find Spam")
+        return;
     }
 
     console.log(`Welcome to the server, ${member}`);
@@ -45,6 +55,7 @@ bot.on('message', msg => {
         } else {
             msg.reply('HELLO FRIEND');
             // Adds the user to the set so that they can't talk for a minute
+            //console.log(msg.member);
             commandCooldown.add(msg.author.id);
             setTimeout(() => {
                 // Removes the user from the set after a minute
@@ -68,10 +79,75 @@ bot.on('message', msg => {
 */
 
 bot.on('message', msg => {
+    if (msg.member._roles.some(r => r === rolePoutine) && msg.content === "!mongo") {
+        client.connect(async err => {
+            try {
+                const collection = await client.db("game").collection("gameCollection");
+                const outPut = await collection.find().toArray();
+                console.log(outPut)
+                msg.reply(`You have ${outPut[0].points} points`)
+                
+                // perform actions on the collection object
+            }
+            catch (err) {
+                console.log(err);
+            }
+
+            //client.close();
+        });
+    }
+})
+
+bot.on('message', msg => {
+    if (msg.member._roles.some(r => r === rolePoutine) && msg.content === "!mongoAddPoints") {
+
+
+        client.connect(async err => {
+
+           await pointSchema.findOneAndUpdate(
+                {_id: '5f631363aa7d05d7547c6d61' },
+                {points: 10}
+            )
+
+            /*
+            try {
+                const collection = await client.db("game").collection("gameCollection");
+                collection.findOneAndUpdate({
+                    filter: { _id : { $eq : '5f631363aa7d05d7547c6d61' }},
+                    update: { $set : {'points': 10 }}
+                }).then(console.log("foo")).catch(console.log());    
+            }
+            */
+
+            try{
+                const collection = await client.db("game").collection("gameCollection");
+                collection.findOneAndUpdate(
+                    {_id : '5f631363aa7d05d7547c6d61'},
+                    {points : 10}
+                    ).then(console.log('foo'));
+            }
+            catch (err) {
+                console.log(err);
+            }
+            
+
+
+
+            //console.log(outPut)
+            msg.reply(`You have added 10 points`)
+            //perform actions on the collection object
+            //client.close();
+        });
+    }
+})
+
+
+bot.on('message', msg => {
     var d = new Date(); //current time
     //console.log('Here');
     console.log(d.getHours());
-    if (msg.author.id === Tanner && msg.content === "!serverTime") {
+    //console.log(msg.member._roles);
+    if (msg.member._roles.some(r => r === rolePoutine) && msg.content === "!serverTime") {
         //msg.reply('Mock Landon');
         msg.reply(d.getHours());
     }
